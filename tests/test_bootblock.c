f83545b7eff792c798cd28b64f7b9288c91e9e01
pos <pgousdal@users.noreply.github.com>
2026-09-14 17:49:29 +0200

    Fix bootblock checksum fixture on 64-bit hosts

--- tests/test_bootblock.c
+++ tests/test_bootblock.c
@@ -1,7 +1,10 @@
 #include "amiheuristics/bootblock.h"
 
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
+
+#define TEST_U32_MASK 0xffffffffUL
 
 static void write_be32(unsigned char *p, unsigned long v)
 {
@@ -13,28 +16,45 @@ static void write_be32(unsigned char *p, unsigned long v)
 
 static unsigned long read_be32(const unsigned char *p)
 {
-    return ((unsigned long)p[0] << 24) |
-           ((unsigned long)p[1] << 16) |
-           ((unsigned long)p[2] << 8) |
-           (unsigned long)p[3];
+    return (((unsigned long)p[0] << 24) |
+            ((unsigned long)p[1] << 16) |
+            ((unsigned long)p[2] << 8) |
+            (unsigned long)p[3]) & TEST_U32_MASK;
+}
+
+static unsigned long add_u32_carry(unsigned long sum, unsigned long value)
+{
+#if ULONG_MAX > 0xffffffffUL
+    unsigned long total;
+
+    total = (sum & TEST_U32_MASK) + (value & TEST_U32_MASK);
+    sum = total & TEST_U32_MASK;
+    if (total > TEST_U32_MASK)
+        sum = (sum + 1UL) & TEST_U32_MASK;
+    return sum;
+#else
+    unsigned long old_sum;
+
+    old_sum = sum;
+    sum += value;
+    if (sum < old_sum)
+        ++sum;
+    return sum;
+#endif
 }
 
 static unsigned long checksum(const unsigned char *data)
 {
     unsigned long sum;
     unsigned long value;
-    unsigned long old_sum;
     unsigned int i;
 
     sum = 0UL;
     for (i = 0U; i < 256U; ++i) {
         value = (i == 1U) ? 0UL : read_be32(data + i * 4U);
-        old_sum = sum;
-        sum += value;
-        if (sum < old_sum)
-            ++sum;
+        sum = add_u32_carry(sum, value);
     }
-    return ~sum;
+    return (~sum) & TEST_U32_MASK;
 }
 
 static int test_valid(void)
