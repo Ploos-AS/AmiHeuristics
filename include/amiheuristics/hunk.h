e269f472ba4b5befffa0b99f0dc31f1fa6a76bff
pos <pgousdal@users.noreply.github.com>
2026-09-15 01:14:09 +0200

    M2: add HUNK analysis API

---
+++ include/amiheuristics/hunk.h
@@ -0,0 +1,27 @@
+#ifndef AMIHEURISTICS_HUNK_H
+#define AMIHEURISTICS_HUNK_H
+
+#include <stddef.h>
+
+#define AMIHEUR_HUNK_MAX_FINDINGS 16U
+
+typedef struct AmiHeurHunkReport {
+    int valid;
+    unsigned long hunk_count;
+    unsigned long code_bytes;
+    unsigned long data_bytes;
+    unsigned long bss_bytes;
+    unsigned long relocation_hunks;
+    int has_code;
+    int has_data;
+    int has_bss;
+    int has_a6_lvo_call;
+    int score;
+    unsigned long finding_count;
+    const char *finding_ids[AMIHEUR_HUNK_MAX_FINDINGS];
+} AmiHeurHunkReport;
+
+int amiheur_hunk_analyze(const unsigned char *data, size_t size,
+                         AmiHeurHunkReport *report);
+
+#endif
